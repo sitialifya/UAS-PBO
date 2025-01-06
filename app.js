8fd89c6d7576b4632094039933c1766612284fcd
sitialifya <alifyasafrina9@gmail.com>
2025-01-06 09:34:43 -0800

    Update app.js

--- app.js
+++ app.js
@@ -76,12 +76,13 @@ function showReceipt() {
     finalTotalElement.textContent = total.toLocaleString();
 }
 if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/service-worker.js')
-      .then((registration) => {
-        console.log('Service Worker terdaftar dengan sukses:', registration);
-      })
-      .catch((error) => {
-        console.log('Pendaftaran Service Worker gagal:', error);
-      });
-  });
+    window.addEventListener('load', () => {
+        navigator.serviceWorker.register('/service-worker.js')
+        .then((registration) => {
+            console.log('Service Worker terdaftar dengan sukses:', registration);
+        })
+        .catch((error) => {
+            console.log('Pendaftaran Service Worker gagal:', error);
+        });
+    });
+}
